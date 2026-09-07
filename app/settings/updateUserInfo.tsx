6f12957832be3f2af6dcf48734f60a6bd5413a39
dk <dk@dkui-MacBookPro.local>
2026-09-07 13:37:10 +0900

    feat: add image picker

--- app/settings/updateUserInfo.tsx
+++ app/settings/updateUserInfo.tsx
@@ -10,8 +10,9 @@ import {
   SafeAreaContainer,
   TextButton,
 } from "@kukkim/react-native-ui";
+import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
-import { StyleSheet } from "react-native";
+import { Alert, StyleSheet } from "react-native";
 
 export default function UpdateUserInfoScreen() {
   const { t } = useTranslation();
@@ -35,6 +36,40 @@ export default function UpdateUserInfoScreen() {
     });
   };
 
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library.
+    // Manually request permissions for videos on iOS when `allowsEditing` is set to `false`
+    // and `videoExportPreset` is `'Passthrough'` (the default), ideally before launching the picker
+    // so the app users aren't surprised by a system dialog after picking a video.
+    // See "Invoke permissions for videos" sub section for more details.
+
+    try {
+      const permissionResult =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (!permissionResult.granted) {
+        Alert.alert(
+          "Permission required",
+          "Permission to access the media library is required.",
+        );
+        return;
+      }
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ["images", "videos"],
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+
+      console.log(result);
+
+      if (!result.canceled) {
+        setImgUri(result.assets[0].uri);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <SafeAreaContainer>
       <CommonHeader
@@ -54,6 +89,10 @@ export default function UpdateUserInfoScreen() {
         title={t("common.profileImage")}
         value={imgUri}
         onChangeText={setImgUri}
+      />
+      <TextButton
+        title={t("settings.profile.getImageFromLibrary")}
+        onPress={pickImage}
       />
       <CommonInput
         title={t("common.name")}
